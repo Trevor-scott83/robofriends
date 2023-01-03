a19d00ffe8fd38827733555a2b9f15c662ffd31f
Trevor-scott83 <98718168+Trevor-scott83@users.noreply.github.com>
2023-01-02 22:25:26 -0700

    Initialize project using Create React App

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+    
+    return(
+        <div className="bg-light-green dib br4 pa3 ma2 grow bw3 shadow-5">
+            <img alt='robot'  src={`https://robohash.org/${id}?200x200`} />
+            <div>
+             <h1>{name}</h1>
+             <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
